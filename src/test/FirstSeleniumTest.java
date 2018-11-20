d20b87f71e6293be064afe631bdf15cb451f202d
jenasubhadev <jenasubhadev@gmail.com>
2018-11-20 12:36:07 +0530

    Updating The workspace to support Selenium 3

--- src/test/FirstSeleniumTest.java
+++ src/test/FirstSeleniumTest.java
@@ -1,12 +1,46 @@
 package test;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstSeleniumTest {
+
+	static String browser;
+	static WebDriver driver;
+
 	public static void main(String[] args) {
-		WebDriver driver = new FirefoxDriver();
+
+		setBrowser();
+		setBrowserConfig();
+		runTest();
+
+
+	}
+
+	public static void setBrowser(){
+		browser="Firefox";
+
+	}
+	public static void setBrowserConfig(){
+		String projectLocation = System.getProperty("user.dir");
+
+		if(browser.contains("Firefox")){
+			System.setProperty("webdriver.firefox.bin", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+			System.setProperty("webdriver.gecko.driver", projectLocation+"\\lib\\geckodriver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+
+		if(browser.contains("Chrome")){
+			System.setProperty("webdriver.chrome.driver", projectLocation+"\\lib\\chromedriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+	}
+	public static void runTest(){
 		driver.get("https://google.com/");
+		driver.quit();
 	}
 
 }
+
+
